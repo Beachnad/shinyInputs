8c237685f8bcd2b451c699798421411f455d0d86
unknown <DannyBeachnau@gmail.com>
2019-07-11 19:43:58 -0400

    Changed styleing for checboxTextInput

--- R/checkboxTextInput.R
+++ R/checkboxTextInput.R
@@ -3,10 +3,9 @@ checkboxTextInput <- function(id, label){
   html <- tags$div(
     id = id,
     class="siny-input-container checkbox-text-container",
-    tags$input(type="checkbox"),
+    tags$input(type="checkbox", style="margin-right: 4px;"),
     tags$span(label),
     tags$input(type="text", class="other-radio-text-input form-control shiny-bound-input")
   )
-  
   attachShinyInputsDep(html, widget='checkbox text')
 }
