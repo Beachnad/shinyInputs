ded6f64c570367888c199a34db7d902fe1578538
Danny Beachnau <dbeachnau@procarepain.com>
2019-08-12 08:22:45 -0400

    Added value argument

--- R/checkboxTextInput.R
+++ R/checkboxTextInput.R
@@ -1,11 +1,11 @@
 #' @export
-checkboxTextInput <- function(id, label){
+checkboxTextInput <- function(id, label, value=NULL){
   html <- tags$div(
     id = id,
     class="siny-input-container checkbox-text-container",
     tags$input(type="checkbox"),
     tags$span(label),
-    tags$input(type="text", class="other-radio-text-input form-control shiny-bound-input")
+    tags$input(type="text", class="other-radio-text-input form-control shiny-bound-input", value=value)
   )
   
   attachShinyInputsDep(html, widget='checkbox text')
